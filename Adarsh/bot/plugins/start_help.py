048abc74c62a7dd4e70d47b66b9a72e6fe13322b
jkdevil27 <91070111+jkdevil27@users.noreply.github.com>
2022-06-01 17:52:12 +0530

    Update start_help.py

--- Adarsh/bot/plugins/start_help.py
+++ Adarsh/bot/plugins/start_help.py
@@ -77,7 +77,7 @@ async def start(b, m):
             return
     await StreamBot.send_photo(
         chat_id=m.chat.id,
-        photo ="https://telegra.ph/file/0d75705ca2746a1fbc1b8.jpg.jpg",
+        photo ="https://telegra.ph/file/0d75705ca2746a1fbc1b8.jpg",
         caption =f'Hi {m.from_user.mention(style="md")}!,\nI am Telegram File to Link Generator Bot with Channel support.\nSend me any file and get a direct download link and streamable link.!',
         reply_markup=buttonz)
 
